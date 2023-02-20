ff1f3d02a28f2905a643a9a3452fb9a4396a8687
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-20 21:54:39 +0200

    added authentication with bcrypt

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
 
 const bodyParser = require("body-parser");
 
@@ -16,8 +17,61 @@ const client = new MongoClient(uri);
 
 app.use(cors());
 
-app.get("/api", (req, res) => {
-  res.json({ users: ["userOne", "userTwo", "userThree"] });
+// app.get("/users", (req, res) => {
+//   res.json({ users: ["userOne", "userTwo", "userThree"] });
+// });
+
+app.get("/users", async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con
+      .db("social_media")
+      .collection("users")
+      .find()
+      .toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = { name: req.body.name, password: hashedPassword };
+    const con = await client.connect();
+    const data = await con
+      .db("social_media")
+      .collection("users")
+      .insertOne(user);
+    await con.close();
+    res.send(data);
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.post("/user/login", async (req, res) => {
+  try {
+    const con = await client.connect();
+    const user = await con
+      .db("social_media")
+      .collection("users")
+      .findOne({ name: req.body.name });
+    await con.close();
+    if (user === null) {
+      return res.status(400).send("Cannot find user");
+    }
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Success");
+    } else {
+      res.send("Not Allowed");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
 });
 
 app.listen(5000, () => {
