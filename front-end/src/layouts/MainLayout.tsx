8cbfa9ca9f3fde8459383d3d78dab7521ee584e1
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-22 00:15:36 +0200

    mainlayout style updates

--- front-end/src/layouts/MainLayout.tsx
+++ front-end/src/layouts/MainLayout.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react";
+import styles from "./MainLayout.module.scss";
 
 interface props {
   children: ReactNode;
 }
 
 const MainLayout: React.FC<props> = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className={styles.container}>{children}</div>;
 };
 
 export default MainLayout;
