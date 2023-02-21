a6c0a0ef74245a1ac550391aff255b0170e44d8a
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-22 00:14:18 +0200

    button component

--- front-end/src/components/Button/Button.tsx
+++ front-end/src/components/Button/Button.tsx
@@ -1,12 +1,23 @@
 import { ReactNode } from "react";
+import classNames from "classnames";
+import styles from "./Button.module.scss";
+import { buttonClassNames } from "../../consts/buttonClassNames";
+
+const cn = classNames.bind(styles);
 
 interface props {
   children: ReactNode;
   onClick: () => void;
+  variant: string;
 }
 
-const Button: React.FC<props> = ({ children, onClick }) => {
-  return <button onClick={onClick}>{children}</button>;
+const Button: React.FC<props> = ({ children, variant, onClick }) => {
+  const classes = buttonClassNames[variant];
+  return (
+    <button className={cn(...classes)} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
