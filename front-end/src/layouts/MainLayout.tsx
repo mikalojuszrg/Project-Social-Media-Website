8163e50ec92003bc7d87a04fba9a14e7f036e9b7
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-22 23:47:11 +0200

    footer added

--- front-end/src/layouts/MainLayout.tsx
+++ front-end/src/layouts/MainLayout.tsx
@@ -6,7 +6,14 @@ interface props {
 }
 
 const MainLayout: React.FC<props> = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={styles.container}>
+      {children}
+      <footer className={styles.container__footer}>
+        Created by Mikalojus Žirgulis
+      </footer>
+    </div>
+  );
 };
 
 export default MainLayout;
