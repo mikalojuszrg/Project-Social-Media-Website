0d05cc9ad01e393bce651f2c562eaacf48e17ae1
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-26 23:36:17 +0200

    updated user apis

--- front-end/src/api/user.ts
+++ front-end/src/api/user.ts
@@ -1,19 +1,21 @@
 import axios from "axios";
+import { User, UserLogin, UserRegister } from "../types/user";
 
 const USERS_API_URL = "http://localhost:5000/users";
 const USER_LOGIN_API_URL = "http://localhost:5000/user/login";
 
-export const createUser = async (user: any): Promise<any> => {
+export const createUser = async (user: UserRegister): Promise<User> => {
   const response = await axios.post(USERS_API_URL, user);
   return response.data.data;
 };
 
-export const fetchUser = async (): Promise<any> => {
+export const fetchUser = async (): Promise<User> => {
   const response = await axios.get(USERS_API_URL);
-  return response.data.data;
+  return response.data;
 };
 
-export const loginUser = async (user: any): Promise<any> => {
+export const loginUser = async (user: UserLogin): Promise<User> => {
   const response = await axios.post(USER_LOGIN_API_URL, user);
+  console.log(response.data);
   return response.data.data;
 };
