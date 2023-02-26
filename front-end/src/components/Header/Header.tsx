70ba90cbc04cd4cef3ad84fe2b3ea4bb1b4f3a8c
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-26 23:36:36 +0200

    made header dynamic

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
-import { useNavigate } from "react-router-dom";
-import { REGISTER_PATH } from "../../routes/const";
+import { useLocation, useNavigate } from "react-router-dom";
+import { HOME_PATH, REGISTER_PATH } from "../../routes/const";
 import Button from "../Button/Button";
 import Logo from "../Logo/Logo";
 import Navbar from "../Navbar/Navbar";
@@ -7,17 +7,25 @@ import styles from "./Header.module.scss";
 
 const Header = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <header className={styles.header}>
-      <Logo />
-      <Navbar />
+      <Logo onClick={() => navigate(HOME_PATH)} />
+      {location.pathname === HOME_PATH ? <Navbar /> : <></>}
       <div className={styles.header__buttons}>
         <Button variant="secondary" onClick={() => console.log("Yo")}>
           Contact
         </Button>
-        <Button variant="primary" onClick={() => navigate(REGISTER_PATH)}>
-          SIGN UP
+        <Button
+          variant="primary"
+          onClick={() =>
+            navigate(
+              location.pathname === HOME_PATH ? REGISTER_PATH : HOME_PATH
+            )
+          }
+        >
+          {location.pathname === HOME_PATH ? "SIGN UP" : "LOGIN"}
         </Button>
       </div>
     </header>
