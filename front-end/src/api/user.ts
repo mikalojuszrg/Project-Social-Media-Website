aa8c8055c9d0d8254523a73e5b8f9d08e0bf4ac7
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-26 17:15:14 +0200

    users fetch

--- front-end/src/api/user.ts
+++ front-end/src/api/user.ts
@@ -1,1 +1,19 @@
-export const fetchUser = () => {};
+import axios from "axios";
+
+const USERS_API_URL = "http://localhost:5000/users";
+const USER_LOGIN_API_URL = "http://localhost:5000/user/login";
+
+export const createUser = async (user: any): Promise<any> => {
+  const response = await axios.post(USERS_API_URL, user);
+  return response.data.data;
+};
+
+export const fetchUser = async (): Promise<any> => {
+  const response = await axios.get(USERS_API_URL);
+  return response.data.data;
+};
+
+export const loginUser = async (user: any): Promise<any> => {
+  const response = await axios.post(USER_LOGIN_API_URL, user);
+  return response.data.data;
+};
--- front-end/src/pages/Feed/Feed.tsx
+++ front-end/src/pages/Feed/Feed.tsx
@@ -1,4 +1,11 @@
+import { useUsers } from "../../hooks/user";
+
 const Feed = () => {
+  const { data } = useUsers();
+  const users = data || [];
+
+  console.log(users);
+
   return <div>Feed</div>;
 };
 
