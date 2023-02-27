351fa52f3748a7b0a280657d74c852b98f611498
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-27 13:17:57 +0200

    added user context

---
+++ front-end/src/contexts/UserContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, useState, ReactNode } from "react";
+import { User } from "../types/user";
+
+type UserContextType = {
+  user: User | null; // Allow the user to be null initially
+  setUser: (user: User | null) => void;
+};
+
+const UserContext = createContext<UserContextType>({
+  user: null,
+  setUser: () => {},
+});
+
+type UserProviderProps = {
+  children: ReactNode;
+};
+
+const UserProvider = ({ children }: UserProviderProps) => {
+  const [user, setUser] = useState<User | null>(null); // Update the state type
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserContext, UserProvider };
--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
+import { UserProvider } from "./contexts/UserContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,12 +16,14 @@ const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <Toaster />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <Toaster />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </UserProvider>
   </React.StrictMode>
 );
 
