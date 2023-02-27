74fc130b62c8608f4b4aed46fcbacea0b4e16bc7
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-27 15:47:10 +0200

    implemented post form into feed

--- front-end/src/pages/Feed/Feed.tsx
+++ front-end/src/pages/Feed/Feed.tsx
@@ -1,17 +1,29 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import PostForm from "../../components/PostForm/PostForm";
 import { UserContext } from "../../contexts/UserContext";
 import { useUsers } from "../../hooks/user";
+import { HOME_PATH } from "../../routes/const";
 
 const Feed = () => {
+  const navigate = useNavigate();
   const { user } = useContext(UserContext);
+  const { first_name, last_name } = user ?? {};
 
   console.log(user);
   const { data } = useUsers();
   const users = data || [];
 
+  !user && navigate(HOME_PATH);
+
   console.log(users);
 
-  return <div></div>;
+  return (
+    <div>
+      {first_name} {last_name}
+      <PostForm />
+    </div>
+  );
 };
 
 export default Feed;
