0131136c4ba28b61b911ae09fbb40bd8144acd35
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-02-28 14:59:43 +0200

    fetchPosts api added

--- front-end/src/api/posts.ts
+++ front-end/src/api/posts.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
-import { Post } from "../types/posts";
+import { NewPost, Post } from "../types/posts";
 
 const POSTS_API_URL = "http://localhost:5000/posts";
 
-export const createPost = async (post: Post): Promise<Post> => {
+export const createPost = async (post: NewPost): Promise<Post> => {
   const response = await axios.post(POSTS_API_URL, post);
+  return response.data;
+};
+
+export const fetchPost = async (): Promise<Post[]> => {
+  const response = await axios.get(POSTS_API_URL);
   return response.data;
 };
