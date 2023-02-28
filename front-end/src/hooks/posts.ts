7dfcac63da16da9f1cca54fee67561edfaac6414
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-02-28 15:01:30 +0200

    fetchPosts custom hook created

--- front-end/src/hooks/posts.ts
+++ front-end/src/hooks/posts.ts
@@ -1,6 +1,12 @@
-import { useMutation } from "@tanstack/react-query";
-import { createPost } from "../api/posts";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { createPost, fetchPost } from "../api/posts";
+
+const POSTS = "POSTS";
 
 export const useCreatePost = () => {
   return useMutation(createPost);
+};
+
+export const usePosts = () => {
+  return useQuery([POSTS], fetchPost);
 };
