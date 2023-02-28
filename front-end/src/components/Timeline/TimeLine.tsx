8ead0eff1b5434206fa4a090bd72750a24ee6d05
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-02-28 16:07:07 +0200

    added posts to timeline

--- front-end/src/components/Timeline/TimeLine.tsx
+++ front-end/src/components/Timeline/TimeLine.tsx
@@ -1,7 +1,23 @@
+import { usePosts } from "../../hooks/posts";
 import styles from "./Timeline.module.scss";
 
 const TimeLine = () => {
-  return <div>TimeLine</div>;
+  const { data } = usePosts();
+  const posts = data || [];
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div>
+          <p>{post.content}</p>
+          <p>{post.date}</p>
+          <p>
+            {post.first_name} {post.last_name}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TimeLine;
