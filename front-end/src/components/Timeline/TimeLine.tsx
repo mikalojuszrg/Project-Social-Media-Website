f35bd22a4163dfb7902037c5e756b1324393a980
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-03-05 18:59:17 +0200

    added loader to initial posts fetch

--- front-end/src/components/Timeline/TimeLine.tsx
+++ front-end/src/components/Timeline/TimeLine.tsx
@@ -1,21 +1,30 @@
-import { usePosts } from "../../hooks/posts";
+import Loader from "../Loader/Loader";
+import { Post } from "../../types/posts";
+import { reverseArray } from "../../utils/reverseArray";
 import styles from "./Timeline.module.scss";
+import { usePosts } from "../../hooks/posts";
 
 const TimeLine = () => {
-  const { data } = usePosts();
+  const { data, isLoading } = usePosts();
   const posts = data || [];
+
+  const reversedPosts = reverseArray(posts);
 
   return (
     <div>
-      {posts.map((post) => (
-        <div>
-          <p>{post.content}</p>
-          <p>{post.date}</p>
-          <p>
-            {post.first_name} {post.last_name}
-          </p>
-        </div>
-      ))}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        reversedPosts.map((post: Post) => (
+          <div>
+            <p>{post.content}</p>
+            <p>{post.date}</p>
+            <p>
+              {post.first_name} {post.last_name}
+            </p>
+          </div>
+        ))
+      )}
     </div>
   );
 };
