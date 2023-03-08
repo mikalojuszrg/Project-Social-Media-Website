1c86f5434ba7233795b3653c7095198d22547b33
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-03-08 21:56:19 +0200

    added handledelete func

--- front-end/src/components/PostCard/PostCard.tsx
+++ front-end/src/components/PostCard/PostCard.tsx
@@ -1,8 +1,11 @@
+import Button from "../Button/Button";
+import { FaUserCircle } from "react-icons/fa";
 import { Post } from "../../types/posts";
 import { UserContext } from "../../contexts/UserContext";
 import { formatDate } from "../../utils/formatDate";
 import styles from "./PostCard.module.scss";
 import { useContext } from "react";
+import { useDeletePost } from "../../hooks/posts";
 
 type Props = {
   post: Post;
@@ -11,16 +14,32 @@ type Props = {
 const PostCard: React.FC<Props> = ({ post }) => {
   const { user } = useContext(UserContext);
   const { email } = user ?? {};
-
+  const { mutateAsync: deletePost } = useDeletePost();
   const loggedUserPost = post.email === email;
 
+  const handleDelete = async (id: number) => {
+    await deletePost(id);
+  };
+
   return (
-    <div className={loggedUserPost ? styles.container : styles.con}>
-      <p className={styles.container__name}>
-        {post.first_name} {post.last_name}
-      </p>
-      <p>{post.content}</p>
+    <div className={styles.container}>
+      <div className={styles.container__title}>
+        <FaUserCircle
+          className={
+            loggedUserPost
+              ? styles.container__logged
+              : styles.container__unlogged
+          }
+        />
+        <p className={styles.container__name}>
+          {post.first_name} {post.last_name}
+        </p>
+      </div>
+      <p className={styles.container__content}>{post.content}</p>
       <p>{formatDate(post.date)}</p>
+      <Button variant="primary" onClick={() => handleDelete(post.id)}>
+        Delete
+      </Button>
     </div>
   );
 };
