fbf3da91cfc814e86f1200bf39f9f7fb5a2279ed
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-03-08 21:55:34 +0200

    created delete request with axios

--- front-end/src/api/posts.ts
+++ front-end/src/api/posts.ts
@@ -1,5 +1,6 @@
-import axios from "axios";
 import { NewPost, Post } from "../types/posts";
+
+import axios from "axios";
 
 const POSTS_API_URL = "http://localhost:5000/posts";
 
@@ -10,5 +11,10 @@ export const createPost = async (post: NewPost): Promise<Post> => {
 
 export const fetchPost = async (): Promise<Post[]> => {
   const response = await axios.get(POSTS_API_URL);
+  return response.data;
+};
+
+export const deletePost = async (id: number): Promise<Post> => {
+  const response = await axios.delete(`${POSTS_API_URL}/${id}`);
   return response.data;
 };
