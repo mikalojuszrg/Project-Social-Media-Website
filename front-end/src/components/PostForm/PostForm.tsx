25ea4acb45da0a574494b724202309589c4d9b3e
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-03-12 11:27:06 +0200

    add image functionality

--- front-end/src/components/PostForm/PostForm.tsx
+++ front-end/src/components/PostForm/PostForm.tsx
@@ -1,17 +1,21 @@
 import * as Yup from "yup";
 
-import { Field, Form, Formik } from "formik";
+import { Field, Form, Formik, FormikHelpers } from "formik";
 import { useContext, useState } from "react";
 import { useCreatePost, usePosts } from "../../hooks/posts";
 
 import Button from "../Button/Button";
-import { FormikHelpers } from "formik";
 import { NewPost } from "../../types/posts";
 import { UserContext } from "../../contexts/UserContext";
 import styles from "./PostForm.module.scss";
 
 const validationSchema = Yup.object().shape({
   content: Yup.string().required("Your post can't be empty. Write something!"),
+  image: Yup.mixed().test(
+    "fileSize",
+    "Image file size should be less than 2MB",
+    (value: any) => !value || (value && (value as File).size <= 2000000)
+  ),
 });
 
 const PostForm = () => {
@@ -35,27 +39,47 @@ const PostForm = () => {
     setIsFetching(false);
   };
 
+  const handleImageChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    setFieldValue: (field: string, value: any) => void
+  ) => {
+    const file = event.currentTarget.files?.[0];
+    if (file) {
+      setFieldValue("image", file);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Formik
-        initialValues={{
-          content: "",
-          email: email,
-          id: new Date().getTime(),
-          first_name: first_name,
-          last_name: last_name,
-          date: "",
-        }}
+        initialValues={
+          {
+            content: "",
+            email: email,
+            id: new Date().getTime(),
+            first_name: first_name,
+            last_name: last_name,
+            date: "",
+            image: null,
+          } as NewPost
+        }
         onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
-        {() => (
+        {({ setFieldValue }) => (
           <Form className={styles.form}>
             <Field
               className={styles.form__textarea}
               name="content"
               placeholder="Share what's new in your life!"
               component="textarea"
+            />
+            <input
+              id="image"
+              name="image"
+              type="file"
+              accept="image/*"
+              onChange={(event) => handleImageChange(event, setFieldValue)}
             />
             <Button
               variant="primary"
